95a1bb54201939e245c6f9aa66f3625057e99cb6
Jay <jay.lecavalier@colorado.edu>
2015-07-24 18:58:46 -0600

    CNF for negation, conjunction, and disjunction

--- parserUtils.ml
+++ parserUtils.ml
@@ -29,9 +29,11 @@ module FSet = Set.Make(
   end)
 
 let propositionList : propositionData list ref = ref [] (* The list of propositions we are working with *)
+let clauses : FSet.t list ref = ref [] (* The list of clauses to resolve *)
 let nPropositions : int ref = ref 0 (* number of propositions we're working with *)
 let linenum : int ref  = ref 1 (* The line number we're parsing. *)
 let subformulae = ref FSet.empty (* A set of subformulae *)
+let clause = ref FSet.empty (* The current clause *)
 let fhtbl = Hashtbl.create 5 (* Maps formulae to their set of subformulae *)
 
 (* Notify the user of errors. *)
@@ -41,7 +43,6 @@ let fatal str = fprintf stderr "@L%d: Fatal: %s \n" !linenum str; assert false
 (* Prints a formula to the terminal screen *)
 let display_wff (f : parseTree) : unit =
   (*printf "The formula at line %d is:\n\n" !linenum;*)
-  printf "\n\n";
   let rec display_wff_helper (f : parseTree) : string =
     match f with 
     | Empty () -> sprintf ""
@@ -57,7 +58,22 @@ let display_wff (f : parseTree) : unit =
       sprintf "%s%s%s%s%s" (lside) (display_wff_helper node.lchild) (node.valstr) (display_wff_helper node.rchild) (rside)
       end
   in
-  printf "%s\n\n" (display_wff_helper f)
+  printf "%s\n" (display_wff_helper f)
+
+let rec wff_to_string (f : parseTree) : string =
+  match f with
+  | Empty () -> sprintf ""
+  | Parent node -> begin
+  	let (lside, rside) =
+  	  match node.valstr with
+  	  | "=>" -> ("(",")")
+  	  | "<=>" -> ("(",")")
+  	  | "&" -> ("(",")")
+  	  | "|" -> ("(",")")
+  	  | _ -> ("","")
+  	in
+  	sprintf "%s%s%s%s%s" (lside) (wff_to_string node.lchild) (node.valstr) (wff_to_string node.rchild) (rside)
+    end
 
 (* Either TRUE or FALSE *)
 let atomic_subform (b : bool) : parseTree =
@@ -133,3 +149,110 @@ let display_subforms (f : parseTree) : unit =
   in
   printf "\n\nSubformulae:\n\n";
   FSet.iter display_wff subs
+
+let rep (f : parseTree) : parseTree =
+  match f with
+  | Empty () -> assert false
+  | Parent node -> begin
+  	match node.rchild with
+  	| Empty () -> f
+  	| _ -> begin
+  	  let str = (sprintf "rep(%s)" (wff_to_string f)) in
+  	  let pid = !nPropositions in
+      nPropositions := pid + 1;
+  	  let p' = {id = pid; name = str} in
+  	  propositionList := (!propositionList)@[p'];
+  	  Parent { valstr = str; lchild = Empty (); rchild = Empty () }
+  	end
+  end
+
+let display_all_reps (f : parseTree) : unit =
+  printf "Representatives:\n";
+  let subs = try
+    Hashtbl.find fhtbl f
+    with Not_found -> (printf "Formula not found: "; display_wff f; assert false;)
+  in
+  FSet.iter (fun x -> display_wff (rep x)) subs
+
+let update_clauses () : unit =
+  clauses := (!clauses)@[!clause];
+  clause := FSet.empty
+
+let enc (f : parseTree) : parseTree =
+  match f with
+  | Empty () -> assert false
+  | Parent node -> begin
+  	match node.rchild with
+  	| Empty () -> Parent {valstr = "TRUE"; lchild = Empty (); rchild = Empty ()}
+  	| _ -> begin
+  	  match node.valstr with
+  	  | "~" -> begin
+  	  	let rf = rep f in
+  	  	let nrf = Parent {valstr = "~"; lchild = Empty (); rchild = rf} in
+  	  	let rcf = rep node.rchild in
+  	  	let nrcf = Parent {valstr = "~"; lchild = Empty (); rchild = rcf} in
+  	  	clause := FSet.add nrf !clause;
+  	  	clause := FSet.add nrcf !clause;
+  	  	update_clauses ();
+  	  	clause := FSet.add rf !clause;
+  	  	clause := FSet.add rcf !clause;
+  	  	update_clauses ();
+  	  	let c1 = Parent {valstr = "|"; lchild = nrf; rchild = nrcf} in
+  	  	let c2 = Parent {valstr = "|"; lchild = rf; rchild = rcf} in
+  	  	Parent {valstr = "&"; lchild = c1; rchild = c2}
+  	  end
+  	  | "&" -> begin
+        let p = rep f in
+        let np = Parent {valstr = "~"; lchild = Empty (); rchild = p} in
+        let rf1 = rep node.lchild in
+        let rf2 = rep node.rchild in
+        let nrf1 = Parent {valstr = "~"; lchild = Empty (); rchild = rf1} in
+        let nrf2 = Parent {valstr = "~"; lchild = Empty (); rchild = rf2} in
+        clause := FSet.add np !clause;
+        clause := FSet.add rf1 !clause;
+        update_clauses ();
+        clause := FSet.add np !clause;
+        clause := FSet.add rf2 !clause;
+        update_clauses ();
+        clause := FSet.add nrf1 !clause;
+        clause := FSet.add nrf2 !clause;
+        clause := FSet.add p !clause;
+        update_clauses ();
+        let c1 = Parent {valstr = "|"; lchild = np; rchild = rf1} in
+        let c2 = Parent {valstr = "|"; lchild = np; rchild = rf2} in
+        let d1_c3 = Parent {valstr = "|"; lchild = nrf1; rchild = nrf2} in
+        let c3 = Parent {valstr = "|"; lchild = d1_c3; rchild = p} in
+        let conj1 = Parent {valstr = "&"; lchild = c1; rchild = c2} in
+        Parent {valstr = "&"; lchild = conj1; rchild = c3}
+  	  end
+  	  | "|" -> begin
+  	  	let p = rep f in
+  	  	let np = Parent {valstr = "~"; lchild = Empty (); rchild = p} in
+  	  	let rf1 = rep node.lchild in
+  	  	let rf2 = rep node.rchild in
+  	  	let nrf1 = Parent {valstr = "~"; lchild = Empty (); rchild = rf1} in
+        let nrf2 = Parent {valstr = "~"; lchild = Empty (); rchild = rf2} in
+        clause := FSet.add np !clause;
+        clause := FSet.add rf1 !clause;
+        clause := FSet.add rf2 !clause;
+        update_clauses ();
+        let d1_c1 = Parent {valstr = "|"; lchild = np; rchild = rf1} in
+        let c1 = Parent {valstr = "|"; lchild = d1_c1; rchild = rf2} in
+        clause := FSet.add nrf1 !clause;
+        clause := FSet.add p !clause;
+        update_clauses ();
+        let c2 = Parent {valstr = "|"; lchild = nrf1; rchild = p} in
+        clause := FSet.add nrf2 !clause;
+        clause := FSet.add p !clause;
+        update_clauses ();
+        let c3 = Parent {valstr = "|"; lchild = nrf2; rchild = p} in
+        let conj1 = Parent {valstr = "&"; lchild = c1; rchild = c2} in
+        Parent {valstr = "&"; lchild = conj1; rchild = c3}
+  	  end
+  	  | _ -> assert false
+  	end
+  end
+
+let display_enc (f : parseTree) : unit =
+  printf "Encodings:\n";
+  display_wff (enc f)
