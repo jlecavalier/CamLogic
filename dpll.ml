7350d7e5aa2ca4d0c2667a766dc4ea4a218e64ca
Jay LeCavalier <jaylecavalier@Jays-MacBook-Pro.local>
2015-09-25 19:16:42 -0600

    Why don't ocaml sets have a map function :( GRRRR

--- dpll.ml
+++ dpll.ml
@@ -2,19 +2,30 @@ open Printf
 open Set
 open Glbdefs
 
+let false_const = (Parent {valstr = "FALSE"; lchild = Empty (); rchild = Empty ()})
+let true_const = (Parent {valstr = "TRUE"; lchild = Empty (); rchild = Empty ()})
+
 let cleanup (clauses : FSetSet.t) : FSetSet.t * bool =
-	clauses
+  if (FSetSet.mem (FSet.singleton false_const) clauses)
+  then (clauses, true)
+  else begin
+    let cleanup_helper clause =
+      let cleaned_clause = (FSet.remove true_const clause) in
+      FSet.remove false_const cleaned_clause
+    in
+    ((FSetSet.iter cleanup_helper clauses), false)
+  end
 
 let bcp (clauses : FSetSet.t) : FSetSet.t = clauses
 
 let choose_var (clauses : FSetSet.t) : parseTree =
-  Parent {valstr="FALSE";lchild=Empty ();rchild=Empty ();}
+  false_const
 
 let substitute (clauses : FSetSet.t) (atom : parseTree) (value : bool) : FSetSet.t =
   clauses
 
 let rec dpll (clauses : FSetSet.t) : bool =
-  let (cleaned, unsat) = cleanup clauses
+  let (cleaned, unsat) = cleanup clauses in
   if unsat then false else begin
     let clauses' = bcp cleaned in
     if (FSetSet.is_empty clauses') then false
@@ -22,8 +33,8 @@ let rec dpll (clauses : FSetSet.t) : bool =
   	  let clause = (FSetSet.choose clauses') in
   	  if ((FSet.cardinal clause) = 1) then begin
   	    let atom = FSet.choose clause in
-  	    if (atom = (Parent {valstr="FALSE";lchild=Empty ();rchild=Empty ();})) then false
-  	    else if (atom = (Parent {valstr="TRUE";lchild=Empty ();rchild=Empty ();})) then true
+  	    if (atom = false_const) then false
+  	    else if (atom = true_const) then true
   	    else (dpll (substitute clauses' atom true)) || (dpll (substitute clauses' atom false))
   	  end
       else begin
